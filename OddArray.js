a976438ec1f297574f61783e5edea9aedd4b8f8c
Varun V <56495025+created-by-varun@users.noreply.github.com>
2019-10-13 10:09:22 +0530

    Create OddArray.js

---
+++ OddArray.js
@@ -0,0 +1,15 @@
+function findOdd(numbers) {
+    var count = 0;
+    for(var i = 0; i<numbers.length; i++){
+      for(var j = 0; j<numbers.length; j++){
+        if(numbers[i] == numbers[j]){
+          count++;
+        }
+      }
+      if(count % 2 != 0 ){
+        return numbers[i];
+      }
+    }
+  };
+  
+  console.log(findOdd([1,1,2,2,4,4,5,5,5,6,6])); //5
