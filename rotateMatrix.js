1123aa36261e908c2ef83db647e251a0f84a52b4
Varun V <56495025+created-by-varun@users.noreply.github.com>
2020-02-26 17:47:40 +0530

    Updated code for rotating matrix

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,1 +1,32 @@
+const A = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+]
 
+const F = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+function rotate(matrix) {
+  const n = matrix.length;
+  const x = Math.floor(n/ 2);
+  const y = n - 1;
+  for (let i = 0; i < x; i++) {
+     for (let j = i; j < y - i; j++) {
+        k = matrix[i][j];
+        matrix[i][j] = matrix[y - j][i];
+        matrix[y - j][i] = matrix[y - i][y - j];
+        matrix[y - i][y - j] = matrix[j][y - i]
+        matrix[j][y - i] = k
+     }
+  }
+}
+rotate(A);
+console.log(A);
+
+rotate(F);
+console.log(F);
