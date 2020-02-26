43227544442d3e3b487fecf954fee3d89a4a6689
Varun V <56495025+created-by-varun@users.noreply.github.com>
2020-02-26 19:45:42 +0530

    Create RemoveCharFromString.cs

---
+++ RemoveCharFromString.cs
@@ -0,0 +1,32 @@
+using System;
+
+class MainClass { 
+
+static void removeChar(string s,char c) { 
+
+    int j, count = 0, n = s.Length;
+    char[] t = s.ToCharArray(); 
+    
+    for (int i = j = 0; i < n; i++){ 
+        if (s[i] != c) 
+        t[j++] = s[i]; 
+        else
+            count++; 
+    } 
+    while(count > 0){ 
+
+        t[j++] = '\0'; 
+
+        count--; 
+    } 
+    Console.Write(t); 
+}
+
+public static void Main() 
+{ 
+
+    string s = "geeksforgeeks"; 
+
+    removeChar(s, 'g'); 
+} 
+}
